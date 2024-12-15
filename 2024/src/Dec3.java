9d6143e5bccdce194d44452ba6760471c478ed5f
Kapoen <kjasomers@gmail.com>
2024-12-15 16:53:20 +0100

    Part 2 of December 3 2024.

--- 2024/src/Dec3.java
+++ 2024/src/Dec3.java
@@ -1,6 +1,8 @@
+import javax.lang.model.element.Element;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
 import java.util.stream.Stream;
@@ -10,19 +12,32 @@ public class Dec3 {
 
         ArrayList<Integer> allResults = new ArrayList<>();
         Scanner scanner = new Scanner(new File("2024/resources/dec3.txt"));
-        Stream<MatchResult> results = scanner.findAll("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
-        results.forEach(result -> {
-            String[] operation = result.group().split("\\(|,|\\)");
+        Object[] results = scanner.findAll("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)").toArray();
+
+        int total = getValue(results, false);
+        System.out.println("Total: " + total);
+
+        int total2 = getValue(results, true);
+        System.out.println("Total with activations: " + total2);
+    }
+
+    private static int getValue(Object[] results, boolean activation) {
+        int total = 0;
+        boolean activated = true;
+
+        for(Object o: results) {
+            MatchResult matchResult = (MatchResult) o;
+            String[] operation = matchResult.group().split("\\(|,|\\)");
+            if(!(operation.length == 3)) {
+                activated = operation[0].equals("do");
+                continue;
+            }
             int a = Integer.parseInt(operation[1]);
             int b = Integer.parseInt(operation[2]);
-            allResults.add(a * b);
-        });
-
-        int total = 0;
-        for (Integer i: allResults) {
-            total += i;
+            if(!activation || activated) {
+                total += a * b;
+            }
         }
-
-        System.out.println("Total: " + total);
+        return total;
     }
 }
